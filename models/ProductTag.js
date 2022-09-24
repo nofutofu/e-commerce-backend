e16f1c9080a0b0f1afa6603627b12e0052895f56
nofutofu <aaronrweis@gmail.com>
2022-09-24 13:12:43 -0500

    set up the desired models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: false,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        mode: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
