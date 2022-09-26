06f1d22362deb03f7ebd5c623565abeb0c1ed190
nofutofu <aaronrweis@gmail.com>
2022-09-26 17:31:18 -0500

    fixed walkthrough video to meet criteria + comments

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
+// takes user to "Wrong Route!" page if an incorrect route is entered
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
 });
